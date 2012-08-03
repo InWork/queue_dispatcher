2760317ae289950b4effc713d729ee8663c723f9
Philip Kurmann <philip@kman.ch>
2012-08-03 14:32:23 +0200

    Better calculation of two RcAndMsg-classes

--- lib/queue_dispatcher/rc_and_msg.rb
+++ lib/queue_dispatcher/rc_and_msg.rb
@@ -3,7 +3,7 @@ module QueueDispatcher
     attr_accessor :rc, :output, :error_msg
 
 
-    def self.good_rc(output, args = {})
+    def self.good_rc(output = '', args = {})
       rc_and_msg = new
       rc_and_msg.good_rc(output, args)
     end
@@ -46,8 +46,8 @@ module QueueDispatcher
     def +(other)
       rc_and_msg = self.clone
       rc_and_msg.rc += other.rc
-      rc_and_msg.output += "\n#{other.output}" if other.output.present?
-      rc_and_msg.error_msg += "\n#{other.error_msg}" if other.error_msg.present?
+      rc_and_msg.output = rc_and_msg.output ? "#{output}\n#{other.output}" : other.output if other.output.present?
+      rc_and_msg.error_msg = rc_and_msg.error_msg ? "#{error_msg}\n#{other.error_msg}" : other.error_msg if other.error_msg.present?
       rc_and_msg
     end
 
